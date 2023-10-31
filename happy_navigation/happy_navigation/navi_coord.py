906c5f6aab9aaef928319f5b5e2813700bb5ab2c
Takuchan <takuchanpp@gmail.com>
2023-10-31 09:56:41 +0900

    Navigationのサービスパッケージを開発

---
+++ happy_navigation/happy_navigation/navi_coord.py
@@ -0,0 +1,12 @@
+import rclpy
+from rclpy.node import Node
+from std_srvs.srv import Empty
+from std_msgs.msg import Float64
+from move_base_msgs.msg import MoveBaseAction,MoveBaseGoal
+
+def main():
+    print('Hi from happy_navigation.')
+
+
+if __name__ == '__main__':
+    main()
--- happy_navigation/setup.py
+++ happy_navigation/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'navi_coord = happy_navigation.navi_coord:main'
         ],
     },
 )
